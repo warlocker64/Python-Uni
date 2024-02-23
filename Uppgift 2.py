a8f0265539e7473ec050eea6db77e822fb73b8e0
warlocker64 <70263816+warlocker64@users.noreply.github.com>
2024-02-23 20:56:56 +0100

    Update Uppgift 2.py

--- Uppgift 2.py
+++ Uppgift 2.py
@@ -1,14 +1,6 @@
-l1=[76, 23, 45, 12, 54, 9] 
-print("Original List:", l1)
- 
-# sorting list using nested loops
-for i in range(0, len(l1)):
-    for j in range(i+1, len(l1)):
-        if l1[i] >= l1[j]:
-            l1[i], l1[j] = l1[j],l1[i]
- 
-# sorted list
-print("Sorted List", l1)
+people_list = [['Jon','Brazil',21],['Maria','Iceland',38],['Bill','Spain',42],['Liam','Japan',28],['Emma','Italy',51]]
+people_list.sort(key = lambda i: i[2])
+print (people_list)
 
 
 
