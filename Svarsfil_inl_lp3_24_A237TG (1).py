fc7b3b08a83467e5a8c7de5f160900f597013aa4
warlocker64 <70263816+warlocker64@users.noreply.github.com>
2024-03-04 15:33:45 +0100

    4 Mars 2024

--- Svarsfil_inl_lp3_24_A237TG (1).py
+++ Svarsfil_inl_lp3_24_A237TG (1).py
@@ -3,7 +3,6 @@
 # Placera dina modulimpoter här:
 import matplotlib.pyplot as plt
 import csv
-
 def read_file(filnamn):
     pisadata=[]
     with open (filnamn, "r") as file: #Öppnar filen och sedan läser
@@ -11,17 +10,6 @@ def read_file(filnamn):
         for rad in read: #läser filen och använder for sats för att lägga in i en lista
             pisadata.append(rad)
     return pisadata
-
-def nordland(list3):
-
-    land = ["Sweden","Norway","Denmark","Finland","Iceland"] 
-    länder = [] 
-    for w in land: # skapar for loops för att söka på de länder vi letar efter
-        for rad in list3:
-            if rad[0] == w: 
-                länder.append(rad)
-    return länder
-
 # Deluppgift 1: Funktioner från deluppgift 1 i ordning.
 # Skriv din kod här:
 def menytxt(): #Meny 
@@ -39,18 +27,16 @@ def meny(): # Använder whileloop för att skapa en meny.
         val = int(input()) #frågar efter num och använder if satser "Meny" för att göra beslut
         if val == 1:
             filnamn =input("Ange filnamn eller tryck bara Enter för att använda data.csv: ") or 'pisadata.csv'
-            pisadata= read_file(filnamn)   
 
+            pisadata= read_file(filnamn)
+            print(pisadata[:5])
             
         elif val ==2:
-            pisdatasorted()
+            år2018(pisadata, 13)
             
         elif val ==3:
             arsmedel(pisadata)
-            nordländer = nordland(pisadata)
-            medelvAr = (arsmedel(pisadata))
-            nordtabell(pisadata,medelvAr,nordländer)
-            nordtabellgraf(pisadata,medelvAr,nordländer)
+            nordtabell_print(pisadata)
   
             
         elif val ==4:
@@ -68,25 +54,26 @@ def meny(): # Använder whileloop för att skapa en meny.
 # Deluppgift 2: Funktioner från deluppgift 2 i ordning.
 # Skriv din kod här:
     
-def år2018(l,i,TF): # Använder mig av val så det inte behvös repeteras 
-    pisadata.sort(reverse = TF, key= lambda i:i [1:]) # Sorterar datan från 2018+ använder sedan reverse för att fixa störst till minst
-    for col in pisadata[1:11]: # For sats för att skriva ut data från 1 till 10 
-        year18 = col[0:3]
-        year18.sort(reverse=True)# sorterar störst till minst mellan female och male mellan 2018
-        print(f"{year18[l]}--{year18[i]}") #skriver ut med val om vilken lista som ska ut
-    
-def pisdatasorted():
+def år2018(lista,kol): # Använder mig av val så det inte behvös repeteras
+    l18=[]
+    for rad in lista[2:]:
+        l18.append([rad[0], rad[kol]])
+    sortedlist=sorted(l18,key=lambda x:x[-1])
+    best = sorted(sortedlist[-10:], key=lambda x:x[-1], reverse=True)
+    worst = sortedlist[:10]
     print("De tio länder som hade bäst resultat år 2018")
     print("----------------")
     print("Land   Resultat")
     print("----------------")
-    år2018(0,1,True)
+    for rad in best:
+        print(rad[0],rad[1])
     print("----------------")
     print("De tio länder som hade sämst resultat år 2018")
     print("----------------")
     print("Land   Resultat")
     print("----------------")
-    år2018(0,2,False)
+    for rad in worst:
+        print(rad[0],rad[1])
     print("----------------")
 
 
@@ -98,9 +85,9 @@ def kolumnmedel(list, columnindex):
     
     for row in list[2:]:
         try:
-            value =int(row[columnindex]) 
-            totalsum += value 
-            values += 1 
+            value =int(row[columnindex]) #använder mig av row[col index för att bestäma villken column vi letar i]
+            totalsum += value #använder mig av += för att sum datan vi får för varje column 
+            values += 1 #lägger till i values om det genomförs och beräknar sedan antal som finns i listan
         except ValueError:# om det går inte att ändra så hoppar den över.
             pass
     return round(totalsum/values) #beräknar medelvärdet
@@ -108,59 +95,75 @@ def kolumnmedel(list, columnindex):
 
 def arsmedel(list2):
     armedel= []
-    for years in range(2003, 2019): # söker mellan 2003 till 2019
-        for index,item in enumerate(list2[0]): 
-            if str(years) in item or "medel" in item.lower(): 
-                columnindex2 = index
-                break
-        else:
-            continue # om inte så fortsätts sökningen 
+    for columnindex2 in range (13, len(list2[0])):
         medelvärdet= kolumnmedel(list2, columnindex2) # använder oss av funktionen kolumnmedel för att beräkna medelvärdet 
-        armedel.append(medelvärdet)
+        armedel.append(medelvärdet) # och sedan lägga till den i listan 
     return armedel
 
+def nordtabell(list3,lista4):
 
-def nordtabell(list4,medel,länder):#Skapar Nordtabell 
-    arsmedel2 = medel
-    flipdata = länder
-    data = [] 
-    for i in flipdata:
-        del i[:-6]
-        data.append(i)
-    flipdata2 = [list(row) for row in zip(*data)] #Har den för att matcha kod
+    land = ["Sweden","Norway","Denmark","Finland","Iceland"] #skapar en lista för att söka på länder
+    länder = [] #en lista 
 
-    
+    for w in land: # skapar for loops för att söka på de länder vi letar efter
+        for rad in list3:
+            if rad[0] == w: #söker på rad 0 och sedan lägger till den i listan om det matchar listan
+                länder.append([rad[0],rad[13:len(rad)]])
     år = [2018,2015,2012,2009,2006,2003] 
-    
-
-    print("\nKunskapsutveckling i matematik enligt PISA-undersökningen 2003 – 2018.") 
+    print("\nKunskapsutveckling i matematik enligt PISA-undersökningen 2003 – 2018.") #skriver ut Nordtabell
     print("                               Länder:")
     print("--------------------------------------------------------------------------------------")
     print("År  Sweden Norway Denmark Finland Iceland          Medelvärde alla länder")
     print("--------------------------------------------------------------------------------------")
-    for år, flipdata2, arsmedel2 in zip(år, flipdata2, arsmedel2): 
-        flipdata2_str = ' '.join(number + ' '*3 for number in flipdata2)
-        print(f"{år}   {flipdata2_str}                   {arsmedel2}")
+    for i in range(len(år)):
+        print (år[i], länder[0][i], länder[1][i],länder[2][i],länder[3][i],länder[4][i],länder[5][i],länder[6][i],)
+
+
+def nordtabell_print(list4):
+    arsmedel2 = arsmedel(list4) #lägger in medelvärdet
+    medelvärdet = [] #skapar ny lista för att inte få probelm mede andra uppgiften
+    for jjj in arsmedel2: # for loops för att lägga in ny lista
+        medelvärdet.append(jjj) # lägger i lista
+        
+    flipdata2 = nordtabell(list4) 
+    newlist = []
+    for jj in flipdata2:
+        newlist.append(jj) #samma
+    
+    #skapar år lista för att skriva ut
+
+
+
+    for år,flipdata2,arsmedel2 in zip(år,flipdata2,arsmedel2): # använder mig av zip för struktur
+        print(f"{år} {flipdata2}                 {arsmedel2} ") # och skriver ut värden år nord länderna och medelvärden
+    
+        pass
+    nyår = [2018,2015,2012,2009,2006,2003]  # skapar ny år lista för att inte påverka denna uppgift   
+    länder_int = [[int(x) for x in sublist] for sublist in newlist] # gör nord länderna till ints för att sedan lägga in det i nordgraf
+    länder_int = [list(row) for row in zip(*länder_int)] # använder mig av av for loops för att vänd listan igen för att matcha grafen
     
 
-def nordtabellgraf(list5,medel,länder):
-    arsmedel2 = medel
-    nländer = länder
-    nyår = [2018,2015,2012,2009,2006,2003]
-    print(länder)
-    
-    
-    
-    plt.xlabel("År")
-    plt.ylabel("Poäng")
-    plt.title("PISA-undersökningen 2003 – 2018") 
-    plt.grid(True)
-    plt.legend()
-    plt.show()
+    sweden = länder_int[0] #väljer ut länderna från norden
+    norway = länder_int[1]
+    denmark = länder_int[2]
+    finland = länder_int[3]
+    iceland = länder_int[4]
+    plt.plot(nyår, sweden,label="Swden") # skriver ut år och även land och label
+    plt.plot(nyår, norway,label="Norway")
+    plt.plot(nyår, denmark,label="Denmark")
+    plt.plot(nyår, finland,label="Finland")
+    plt.plot(nyår, iceland,label="Iceland")
+    plt.plot(nyår, medelvärdet,label="medel")
+    plt.xlabel("År")#x namn
+    plt.ylabel("Poäng")# y namn
+    plt.title("PISA-undersökningen 2003 – 2018") #Titel
+    plt.grid(True)#grids.
+    plt.legend()#lägger till en ruta i grafen som visar vilken linje som till hör vilket land
+    plt.show()#visa graf
 
 def kvinna_man(list2):
     listamf = []
-    for row in pisadata:
+    for row in listamf:
         del row[-6:]
         listamf.append(row)
     for j in listamf: # skapar loops för att få värden till ints så det är enklare att jämnföra
@@ -170,7 +173,7 @@ def kvinna_man(list2):
             except ValueError:# om det inte går att göra det till en int så fortsätter den
                 pass
     newlist= []
-    for jj in pisadata:
+    for jj in newlist:
         for ii in range(1, len(jj), 2): #vi använder oss av for loops för att beräkna var 2 column
             kvinna= (jj[ii+1]) #flyttar fram för att få fram kvinna
             män= (jj[ii]) #män
@@ -213,4 +216,3 @@ def battresamre(pisadata, bättre):#Detta definierar en funktion som tar två pa
 # Skriv din kod här:
 # Huvudprogram med Meny från deluppgift 0. Använd menyrubriker enl.uppgiftsbeskrivningen.
 # Skriv din kod här:
-meny()
